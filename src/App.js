cdaeecdd8ffec1bc80eb94b49e32250b55666278
AshutoshPardeshi23 <ashutoshpardeshi23@gmail.com>
2024-07-15 21:34:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <DetaProvider>
+      <Home />
+    </DetaProvider>
+  )
+}
+
+export default App
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+import Hero from './Components/Hero'
+import Roll from './Components/Roll'
+import Content from './Components/Content'
+
+function App() {
+
+    return (
+    <div>
+        <Hero />
+        <Roll />
+        <Content/>
+    </div>
+  )
+}
+
+export default App
